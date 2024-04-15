8a6cca883f33818bc528b0f457b199980ba5781d
hee_!J <heeej920@naver.com>
2024-04-15 23:49:24 +0900

    Feat : Spring 기본 세팅 #1

---
+++ src/main/java/com/sogonsogon/neighclova/NeighclovaApplication.java
@@ -0,0 +1,13 @@
+package com.sogonsogon.neighclova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighclovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighclovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sogonsogon/neighclova/NeighclovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogonsogon.neighclova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighclovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
