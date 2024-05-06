8f50c23cfb1980bd06817ea4201c426ea9a47afe
chaeeun-Han <chaeeun.h911@gmail.com>
2024-05-06 18:04:58 +0900

    feat: Add Jwt and Security (#1)

---
+++ src/main/java/com/sogonsogon/neighclova/provider/JwtProvider.java
@@ -0,0 +1,55 @@
+package com.sogonsogon.neighclova.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${JWT_SECRET}")
+    private String JWT_SECRET;
+
+    // jwt 생성
+    public String create(String email) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        String jwt = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .compact();
+
+        return jwt;
+    }
+
+    // jwt 검증
+    public String validate(String jwt) {
+        String subject = null;
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        try {
+            subject = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return subject;
+    }
+}
