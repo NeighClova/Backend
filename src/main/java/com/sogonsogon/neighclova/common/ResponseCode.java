6c047eb3a5457913719a6c7998c365ab6c9113a5
chaeeun-Han <chaeeun.h911@gmail.com>
2024-05-14 09:37:08 +0900

    feat: Add common response code and message (#4)

--- src/main/java/com/sogonsogon/neighclova/common/ResponseCode.java
+++ src/main/java/com/sogonsogon/neighclova/common/ResponseCode.java
@@ -1,14 +1,20 @@
 package com.sogonsogon.neighclova.common;
 
 public interface ResponseCode {
+    // HTTP Status 200
     String SUCCESS = "SU";
 
-    String VALIDATION_FAILED = "VF";
+    // HTTP Status 400
+    String NOT_EXISTED_USER = "NEU";
     String DUPLICATE_EMAIL = "DE";
 
+    // HTTP Status 401
+    String VALIDATION_FAILED = "VF";
     String SIGN_IN_FAILED = "SF";
     String CERTIFICATE_FAIL = "CF";
+    String AUTHORIZATION_FAIL = "AF";
 
+    // HTTP Status 500
     String MAIL_FAIL = "MF";
     String DATABASE_ERROR = "DBE";
 }
--- src/main/java/com/sogonsogon/neighclova/common/ResponseMessage.java
+++ src/main/java/com/sogonsogon/neighclova/common/ResponseMessage.java
@@ -1,14 +1,20 @@
 package com.sogonsogon.neighclova.common;
 
 public interface ResponseMessage {
+    // HTTP Status 200
     String SUCCESS = "success";
 
-    String VALIDATION_FAILED = "validation failed";
+    // HTTP Status 400
+    String NOT_EXISTED_USER = "This user does not exist.";
     String DUPLICATE_EMAIL = "duplicate email";
 
+    // HTTP Status 401
+    String VALIDATION_FAILED = "validation failed";
     String SIGN_IN_FAILED = "Login information mismatch.";
     String CERTIFICATE_FAIL = "certification failed";
+    String AUTHORIZATION_FAIL = "Authorization Failed.";
 
+    // HTTP Status 500
     String MAIL_FAIL = "mail send failed";
     String DATABASE_ERROR = "database error";
 }
