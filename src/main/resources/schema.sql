bfc386f355c80ec4871609907b82c895a661de2a
chaeeun-Han <chaeeun.h911@gmail.com>
2024-05-24 11:46:44 +0900

    refactor: Modify `News` table character set `utf8mb3` to `utf8mb4` (#10)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `news` (
+    `news_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `place_id` BIGINT,
+    `keyword` VARCHAR(30) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT,
+    `created_at` DATETIME(6),
+    `updated_at` DATETIME(6),
+    FOREIGN KEY (`place_id`) REFERENCES `place` (`place_id`) ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
